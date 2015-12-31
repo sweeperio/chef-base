e71e8559180c76506d3d5dccb1ed229b6fe71c78
pseudomuto <david.muto@gmail.com>
2015-12-31 12:21:13 -0500

    updating licenses and metadata to match flay settings

--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,11 @@
 # rubocop:disable Style/SingleSpaceBeforeFirstArg
 name              "base"
-maintainer        "Sweeper Devs"
+maintainer        "sweeper.io"
 maintainer_email  "developers@sweeper.io"
 license           "mit"
 description       "A cookbook that applies for all machines"
 long_description  "A cookbook that applies for all machines"
-version           "0.3.0"
+version           "0.3.1"
 # rubocop:enable Style/SingleSpaceBeforeFirstArg
 
 supports "ubuntu"
@@ -18,3 +18,7 @@ depends "core", "~> 0.0"
 depends "runit", "~> 1.0"
 depends "sudo", "~> 2.0"
 depends "user", "~> 0.0"
+
+chef_version ">= 12.5" if respond_to?(:chef_version)
+source_url "https://github.com/sweeperio/chef-base" if respond_to?(:source_url)
+issues_url "https://github.com/sweeperio/chef-base/issues" if respond_to?(:issues_url)
