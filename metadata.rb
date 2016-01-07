3be7c213c45a73aa0cfbbc3699f3863a1742db5f
pseudomuto <david.muto@gmail.com>
2016-01-07 16:51:55 -0500

    updating to use chefdk in travis/dev

--- metadata.rb
+++ metadata.rb
@@ -4,20 +4,20 @@ maintainer        "sweeper.io"
 maintainer_email  "developers@sweeper.io"
 license           "mit"
 description       "A cookbook that applies for all machines"
-long_description  "A cookbook that applies for all machines"
+long_description  IO.read(File.join(File.dirname(__FILE__), "README.md"))
 version           "0.3.1"
 # rubocop:enable Style/SingleSpaceBeforeFirstArg
 
 supports "ubuntu"
 
-depends "apt", "~> 2.0"
-depends "ark", "~> 0.0"
-depends "build-essential", "~> 2.0"
-depends "chef-client", "~> 4.0"
-depends "core", "~> 0.0"
-depends "runit", "~> 1.0"
-depends "sudo", "~> 2.0"
-depends "user", "~> 0.0"
+depends "apt",              "~> 2.0"
+depends "ark",              "~> 0.0"
+depends "build-essential",  "~> 2.0"
+depends "chef-client",      "~> 4.0"
+depends "core",             "~> 0.0"
+depends "runit",            "~> 1.0"
+depends "sudo",             "~> 2.0"
+depends "user",             "~> 0.0"
 
 chef_version ">= 12.5" if respond_to?(:chef_version)
 source_url "https://github.com/sweeperio/chef-base" if respond_to?(:source_url)
